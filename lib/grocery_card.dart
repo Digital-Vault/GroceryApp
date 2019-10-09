ad8a9088ec53210147d69ee4392575d6205cb2d7
Jacky Tea <teajacks@outlook.com>
2019-10-09 10:37:00 -0400

    added remove item user interface

--- lib/grocery_card.dart
+++ lib/grocery_card.dart
@@ -23,17 +23,52 @@ class GroceryCard extends StatelessWidget {
             ),
           );
         },
-        child: _buildTile(),
+        child: _buildTile(context),
       ),
     );
   }
 
-  Widget _buildTile() {
+  Widget _buildTile(BuildContext context) {
     return ListTile(
-      trailing: Text("${item.quantity}x"),
+      trailing: RaisedButton(
+      child: Icon(Icons.delete, color: Colors.white,),
+      color: Colors.red,
+      shape: CircleBorder(),
+      onPressed: () {
+        confirmItemDeletion(context);
+      },),
+      leading: Text("${item.quantity}x"),
       title: Text(
         item.name,
       ),
+    );
+  }
+
+  void confirmItemDeletion(BuildContext context) {
+    var confirmationDialog = AlertDialog(
+      title: Text("Delete This Item?"),
+      content: Text("Are you sure you want to delete this item? ${item.name} Quantity: ${item.quantity}x"),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("Yeet it"),
+          onPressed: () {
+            //delete functionality here...
+          },
+        ),
+        FlatButton(
+          child: Text("Nah, fam"),
+          onPressed: () {
+            Navigator.pop(context); 
+          },
+        )
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return confirmationDialog;
+      }
     );
   }
 }
