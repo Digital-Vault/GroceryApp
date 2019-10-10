90d9021cf976bf1b926daac2667e234636912a3e
Jacky Tea <teajacks@outlook.com>
2019-10-09 20:49:48 -0400

    swipe delete added

--- lib/detailed_page.dart
+++ lib/detailed_page.dart
@@ -15,14 +15,45 @@ class DetailedPage extends StatelessWidget {
         body: Column(children: <Widget>[
           DetailCard(
             item: item,
-          ), 
+          ),
           RaisedButton(
-            child: Text("Delete Item", style: TextStyle(color: Colors.white),),
+            child: Text(
+              "Delete Item",
+              style: TextStyle(color: Colors.white),
+            ),
             color: Colors.red,
             onPressed: () {
-              Navigator.pop(context);
+              confirmItemDeletion(context);
             },
           )
         ]));
+  }
+
+  void confirmItemDeletion(BuildContext context) {
+    var confirmationDialog = AlertDialog(
+      title: Text("Delete This Item?"),
+      content: Text(
+          "Are you sure you want to delete this item? ${item.name} Quantity: ${item.quantity}x"),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("Yes", style: TextStyle(color: Colors.green),),
+          onPressed: () {
+            //delete functionality here...
+          },
+        ),
+        FlatButton(
+            child: Text("No", style: TextStyle(color: Colors.red),),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return confirmationDialog;
+        });
   }
 }
--- lib/grocery_card.dart
+++ lib/grocery_card.dart
@@ -29,46 +29,27 @@ class GroceryCard extends StatelessWidget {
   }
 
   Widget _buildTile(BuildContext context) {
-    return ListTile(
-      trailing: RaisedButton(
-      child: Icon(Icons.delete, color: Colors.white,),
-      color: Colors.red,
-      shape: CircleBorder(),
-      onPressed: () {
-        confirmItemDeletion(context);
-      },),
-      leading: Text("${item.quantity}x"),
-      title: Text(
-        item.name,
+    return Dismissible(
+      key: ObjectKey(item.name),
+      background: deleteGroceryBackground(),
+      child: ListTile(
+        trailing: Text("${item.quantity}x"),
+        title: Text(
+          item.name,
+        ),
       ),
     );
   }
 
-  void confirmItemDeletion(BuildContext context) {
-    var confirmationDialog = AlertDialog(
-      title: Text("Delete This Item?"),
-      content: Text("Are you sure you want to delete this item? ${item.name} Quantity: ${item.quantity}x"),
-      actions: <Widget>[
-        FlatButton(
-          child: Text("Yeet it"),
-          onPressed: () {
-            //delete functionality here...
-          },
-        ),
-        FlatButton(
-          child: Text("Nah, fam"),
-          onPressed: () {
-            Navigator.pop(context); 
-          },
-        )
-      ],
-    );
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return confirmationDialog;
-      }
+  Widget deleteGroceryBackground() {
+    return Container(
+      alignment: Alignment.centerRight,
+      padding: EdgeInsets.only(right: 20.0),
+      color: Colors.red,
+      child: Icon(
+        Icons.delete,
+        color: Colors.white,
+      ),
     );
   }
 }
