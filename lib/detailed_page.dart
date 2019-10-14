15116179e724ed29b9556f08aeb96e933e42802b
Sang Min Park <9714park@gmail.com>
2019-10-14 12:15:24 -0400

    Add FAB and modified submisson form
    Add a floating action button (FAB) to the main list. Also modified the
    form to accept adding new item.

--- lib/detailed_page.dart
+++ lib/detailed_page.dart
@@ -1,49 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/detail_card.dart';
 import 'package:grocery_app/grocery_item.dart';
-import 'package:grocery_app/edit_item.dart';
+import 'package:grocery_app/submission_form.dart';
 import 'package:grocery_app/item_provider.dart';
 import 'package:grocery_app/main.dart';
+
 class DetailedPage extends StatelessWidget {
   final GroceryItem item;
   DetailedPage({this.item});
 
   @override
   Widget build(BuildContext context) {
-    return  WillPopScope(
-    onWillPop: () async => false,
-    child: Scaffold(
-        appBar: AppBar(
-          title: Text('${item.name} Details'),
-          leading:  IconButton(
-            icon:  Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MyApp(),
-                ),
-              );
-              },
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                final itemBloc = ItemProvider.of(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EditForm(item: item),
-                  ),
-                );
-                itemBloc.removeItem.add(item);
-              },
-            )
-          ],
-        ),
-        body: DetailCard(
-          item: item,
-        )));
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('${item.name} Details'),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    final itemBloc = ItemProvider.of(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SubmissionForm(item: item),
+                      ),
+                    );
+                    itemBloc.removeItem.add(item);
+                  },
+                )
+              ],
+            ),
+            body: DetailCard(
+              item: item,
+            )));
   }
 }
