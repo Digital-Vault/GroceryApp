98fb5d00bc4596f700092e3c02d884861b7c219c
Sang Min Park <9714park@gmail.com>
2019-10-14 12:52:01 -0400

    Update Form to accept an empty GroceryList
    Update submission form to accept an empty grocery item. This allows
    the form to be used for both editing as well as adding new items.

--- lib/submission_form.dart
+++ lib/submission_form.dart
@@ -13,121 +13,75 @@ class SubmissionForm extends StatefulWidget {
 }
 
 class _SubmissionFormState extends State<SubmissionForm> {
-  final GroceryItem item;
+  GroceryItem item;
   _SubmissionFormState({this.item});
   final formKey = GlobalKey<FormState>();
   String _name;
   num _quantity;
   final format = DateFormat("yyyy-MM-dd");
   DateTime _date;
+  String _appBarTitle;
   @override
   Widget build(BuildContext context) {
-    if (item != null) {
-      // Submission form for updating item
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Update Item '),
-          ),
-          body: Card(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextFormField(
-                      initialValue: item.name,
-                      decoration: InputDecoration(labelText: 'Item Name:'),
-                      onSaved: (input) => _name = input,
-                    ),
-                    TextFormField(
-                      initialValue: item.quantity.toString(),
-                      decoration: InputDecoration(labelText: 'Quantity:'),
-                      onSaved: (input) => _quantity = int.parse(input),
-                    ),
-                    DateTimeField(
-                      format: format,
-                      initialValue: item.expiryDate,
-                      decoration: InputDecoration(labelText: 'Date'),
-                      onShowPicker: (context, currentValue) {
-                        return showDatePicker(
-                            context: context,
-                            firstDate: DateTime(1900),
-                            initialDate: currentValue ?? DateTime.now(),
-                            lastDate: DateTime(2100));
-                      },
-                      onSaved: (input) => _date = input,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            onPressed: _submit,
-                            child: Text('Save'),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ));
+    if (item == null) {
+      _appBarTitle = "Add Item";
+      item = GroceryItem(name: "", expiryDate: DateTime.now());
     } else {
-      // Submission form for adding item
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Add Item '),
-          ),
-          body: Card(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Item Name:'),
-                      onSaved: (input) => _name = input,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Quantity:'),
-                      onSaved: (input) => _quantity = int.parse(input),
-                    ),
-                    DateTimeField(
-                      format: format,
-                      decoration: InputDecoration(labelText: 'Date'),
-                      onShowPicker: (context, currentValue) {
-                        return showDatePicker(
-                            context: context,
-                            firstDate: DateTime(1900),
-                            initialDate: currentValue ?? DateTime.now(),
-                            lastDate: DateTime(2100));
-                      },
-                      onSaved: (input) => _date = input,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            onPressed: _submit,
-                            child: Text('Save'),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+      _appBarTitle = "Update Item";
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_appBarTitle),
+        ),
+        body: Card(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    initialValue: item.name,
+                    decoration: InputDecoration(labelText: 'Item Name:'),
+                    onSaved: (input) => _name = input,
+                  ),
+                  TextFormField(
+                    initialValue: item.quantity.toString(),
+                    decoration: InputDecoration(labelText: 'Quantity:'),
+                    onSaved: (input) => _quantity = int.parse(input),
+                  ),
+                  DateTimeField(
+                    format: format,
+                    initialValue: item.expiryDate,
+                    decoration: InputDecoration(labelText: 'Date'),
+                    onShowPicker: (context, currentValue) {
+                      return showDatePicker(
+                          context: context,
+                          firstDate: DateTime(1900),
+                          initialDate: currentValue ?? DateTime.now(),
+                          lastDate: DateTime(2100));
+                    },
+                    onSaved: (input) => _date = input,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton(
+                          onPressed: _submit,
+                          child: Text('Save'),
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-          ));
-    }
+          ),
+        ));
   }
 
   void _submit() {
