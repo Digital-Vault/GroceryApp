fbf7ecf401ccea99b57c4f43a51cf5d79f6b9204
Abiram <abiramks@gmail.com>
2019-10-15 20:54:07 -0400

    changed quantity to textfield

--- lib/submission_form.dart
+++ lib/submission_form.dart
@@ -16,6 +16,7 @@ class SubmissionForm extends StatefulWidget {
 class _SubmissionFormState extends State<SubmissionForm> {
   GroceryItem item;
   _SubmissionFormState({this.item});
+  num _quantity;
   final formKey = GlobalKey<FormState>();
   String _name;
   final format = DateFormat("yyyy-MM-dd");
@@ -44,13 +45,27 @@ class _SubmissionFormState extends State<SubmissionForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
-
                     initialValue: item.name,
                     decoration: InputDecoration(labelText: 'Item Name:'),
                     validator: (input) => input.isEmpty ? 'Name must be entered' : null,
                     onSaved: (input) => _name = input,
                   ),
+                  TextFormField(
+                    initialValue: item.quantity.toString(),
+                    decoration: InputDecoration(labelText: 'Quantity:'),
+                    validator: (input) {
+                      if (input.isEmpty){
+                        return('Quantity cannot be empty');
+                      } else if(int.parse(input)  <= 0){
+                        return("Quantity has to be more than 0");
+                      } else {
+                        return null;
+                      }
+                    },
 
+
+                    onSaved: (input) => _quantity = int.parse(input),
+                  ),
               DateTimeField(
                     format: format,
                     initialValue: item.expiryDate,
@@ -59,32 +74,23 @@ class _SubmissionFormState extends State<SubmissionForm> {
                       return showDatePicker(
                           context: context,
                           firstDate: DateTime(1900),
-                          initialDate: currentValue ?? DateTime.now(),
+                          initialDate: DateTime.now(),
                           lastDate: DateTime(2100));
                     },
-                    validator: (input) => input.toString() == "null" ? 'Date must be entered' : null,
+                    validator: (input){
+                      if (input.toString() == "null"){
+                        return('Date must be entered');
+                      } else if(input.isBefore(DateTime.now())){
+                        return('Date must be in the future');
+                      }else {
+                        return(null);
+                      }
+
+                    },
                     onSaved: (input) => _date = input,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child:  Slider(
-                      min: 1.0,
-                      max: 50.0,
-                      divisions: 100,
-                      value: sliderValue,
-                      label: 'Quantity',
-                      onChanged: (newValue){
-                        setState(() {
-                          sliderValue = newValue;
-                        });
-
-                      },
-                    ),
-                  ),
 
 
-                  Text("Quantity : ${sliderValue.round()}",
-                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
@@ -112,7 +118,7 @@ class _SubmissionFormState extends State<SubmissionForm> {
       GroceryItem EditedItem = GroceryItem(
         name: _name,
         expiryDate: _date,
-        quantity: sliderValue.round(),
+        quantity: _quantity,
         purchased: false,
       );
       itemBloc.addItem.add(EditedItem);
