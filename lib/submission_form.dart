c64952d62a5dec31f291caf764d151dbaa45eb9e
Abiram <abiramks@gmail.com>
2019-10-16 13:41:17 -0400

    removed unused import

--- lib/submission_form.dart
+++ lib/submission_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/detailed_page.dart';
 import 'package:grocery_app/grocery_item.dart';
 import 'package:grocery_app/main.dart';
 import 'package:intl/intl.dart';
