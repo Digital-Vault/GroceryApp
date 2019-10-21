ec5c73fb1e95238d74882ad35f6e3c8cefdba338
Sang Min Park <9714park@gmail.com>
2019-10-21 17:22:54 -0400

    Fix date picker in submission form
    Fix the date picker in submission form such that tapping on the date field
    opens up the date picker. This was not the case previously and the user
    had to tap on the X to remove the date first.

--- lib/submission_form.dart
+++ lib/submission_form.dart
@@ -13,15 +13,27 @@ class SubmissionForm extends StatefulWidget {
 }
 
 class _SubmissionFormState extends State<SubmissionForm> {
-  GroceryItem item;
   _SubmissionFormState({this.item});
-  num _quantity;
-  final formKey = GlobalKey<FormState>();
-  String _name;
-  final format = DateFormat("yyyy-MM-dd");
-  DateTime _date;
+
+  GroceryItem item;
+  String _itemName;
+  num _itemQty;
+  DateTime _itemExpDate;
+  bool _datePicked = false;
+  final String _dateFormat = "yyyy-MM-dd";
   String _appBarTitle;
-  var sliderValue = 1.0;
+  final myController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  var _sliderValue = 1.0;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     if (item == null) {
@@ -39,72 +51,91 @@ class _SubmissionFormState extends State<SubmissionForm> {
   }
 
   Widget _formCard(BuildContext context) => Card(
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextFormField(
-                  initialValue: item.name,
-                  decoration: InputDecoration(labelText: 'Item Name:'),
-                  validator: (input) =>
-                      input.isEmpty ? 'Name must be entered' : null,
-                  onSaved: (input) => _name = input,
-                ),
-                TextFormField(
-                  initialValue: item.quantity.toString(),
-                  decoration: InputDecoration(labelText: 'Quantity:'),
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return ('Quantity cannot be empty');
-                    } else if (int.parse(input) <= 0) {
-                      return ("Quantity has to be more than 0");
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (input) => _quantity = int.parse(input),
-                ),
-                DateTimeField(
-                  format: format,
-                  initialValue: item.expiryDate,
-                  decoration: InputDecoration(labelText: 'Date'),
-                  onShowPicker: (context, currentValue) {
-                    return showDatePicker(
-                        context: context,
-                        firstDate: DateTime(1900),
-                        initialDate: DateTime.now(),
-                        lastDate: DateTime(2100));
-                  },
-                  validator: (input) {
-                    if (input.toString() == "null") {
-                      return ('Date must be entered');
-                    } else if (input.isBefore(DateTime.now())) {
-                      return ('Date must be in the future');
-                    } else {
-                      return (null);
-                    }
-                  },
-                  onSaved: (input) => _date = input,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RaisedButton(
-                        onPressed: _submit,
-                        child: Text('Save'),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
+        child: Padding(padding: EdgeInsets.all(8.0), child: _form()),
+      );
+
+  Widget _form() => Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _itemNameInput(),
+            _itemQuantityInput(),
+            _itemExpiryDate(),
+            _saveButtonRow(),
+          ],
         ),
+      );
+
+  Widget _itemNameInput() => TextFormField(
+        initialValue: item.name,
+        decoration: InputDecoration(hintText: 'Item Name:'),
+        validator: (input) => input.isEmpty ? 'Name must be entered' : null,
+        onSaved: (input) => _itemName = input,
+      );
+
+  Widget _itemQuantityInput() => TextFormField(
+        initialValue: item.quantity.toString(),
+        decoration: InputDecoration(hintText: 'Quantity:'),
+        validator: (input) {
+          if (input.isEmpty) {
+            return ('Quantity cannot be empty');
+          } else if (int.parse(input) <= 0) {
+            return ("Quantity has to be more than 0");
+          } else {
+            return null;
+          }
+        },
+        onSaved: (input) => _itemQty = int.parse(input),
+      );
+
+  Widget _itemExpiryDate() {
+    if (!_datePicked) {
+      _itemExpDate = item.expiryDate;
+    }
+    myController.text = DateFormat(_dateFormat).format(_itemExpDate);
+
+    Future _selectDate() async {
+      DateTime picked = await showDatePicker(
+          context: context,
+          firstDate: DateTime(1900),
+          initialDate: DateTime.now(),
+          lastDate: DateTime(2100));
+      if (picked != null) {
+        setState(() {
+          _itemExpDate = picked;
+          _datePicked = true;
+        });
+      }
+    }
+
+    print(_itemExpDate);
+    InkWell inkWell = InkWell(
+      onTap: _selectDate,
+      child: IgnorePointer(
+        child: TextFormField(
+          controller: myController,
+          // initialValue: DateFormat(_dateFormat).format(_itemExpDate),
+          decoration: InputDecoration(hintText: "Expiry date"),
+          onSaved: (String val) {},
+        ),
+      ),
+    );
+
+    return inkWell;
+  }
+
+  Widget _saveButtonRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              onPressed: _submit,
+              child: Text('Save'),
+            ),
+          )
+        ],
       );
 
   void _submit() {
@@ -113,9 +144,9 @@ class _SubmissionFormState extends State<SubmissionForm> {
       final itemBloc = ItemProvider.of(context);
 
       GroceryItem EditedItem = GroceryItem(
-        name: _name,
-        expiryDate: _date,
-        quantity: _quantity,
+        name: _itemName,
+        expiryDate: _itemExpDate,
+        quantity: _itemQty,
         purchased: false,
       );
       itemBloc.addItem.add(EditedItem);
