76ab9ab6c62c89956a3a073a98351bdf0fb98bb9
Abiram <abiramks@gmail.com>
2020-01-28 14:29:40 -0500

    removed unused import

--- lib/fridge_list.dart
+++ lib/fridge_list.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:grocery_app/notification_util.dart';
 import 'detailed_page.dart';
 import 'firestore_provider.dart';
 import 'grocery_item.dart';
