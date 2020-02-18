0c77f45c9fb3b506568adcaee6cd1c1a2600fb2b
Sang Min Park <9714park@gmail.com>
2020-02-17 20:59:20 -0500

    Added dateInput utility Widget .

--- lib/date_util.dart
+++ lib/date_util.dart
@@ -1,5 +1,27 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 String dateFormatYMMDToString(DateTime date) {
   return DateFormat.yMMMd().format(date);
+}
+
+Widget dateInput({label, onSaved}) {
+  return DateTimeField(
+    decoration: InputDecoration(
+      labelText: "Expiry Date",
+    ),
+    format: DateFormat.yMMMd(),
+    onShowPicker: (context, currentValue) async {
+      final today = DateTime.now();
+
+      return showDatePicker(
+        context: context,
+        firstDate: currentValue ?? today,
+        initialDate: currentValue ?? today,
+        lastDate: DateTime(2100),
+      );
+    },
+    onSaved: onSaved,
+  );
 }
