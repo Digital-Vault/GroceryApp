8cedda2198a721ebb46b8977a03603547cc47678
Jacky Tea <teajacks@outlook.com>
2020-03-22 15:18:25 -0400

    removed unsued pedantic import

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:pedantic/pedantic.dart';
 
 abstract class BaseAuth {
   Future<String> SignInWithEmailAndPassword(String email, String password);
