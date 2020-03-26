c1426da79c15fcb6f505760532272165fb9f8700
Sang Min Park <9714park@gmail.com>
2020-03-26 08:44:12 -0400

    Syncrhonous call to delete document reference.

--- lib/screens/grocery/grocery_list.dart
+++ lib/screens/grocery/grocery_list.dart
@@ -255,13 +255,13 @@ class _GroceryList extends State<GroceryList> {
       onDismissed: (direction) async {
         if (groceryItem.expiryDate == null) {
           await _showDialog(context, document);
-          await document.reference.delete();
+          document.reference.delete();
         } else {
           var docRef = await firestore
               .collection(_fridgeCollectionName)
               .add(groceryItem.toJson());
 
-          await document.reference.delete();
+          document.reference.delete();
           await scheduleExpiryNotification(groceryItem.notifyDate,
               groceryItem.expiryDate, groceryItem.name, docRef.documentID);
         }
